f8bd6a56318e653a327f90fa2002be6d4824443b
DTMaguire <Daniel.Maguire@nswlrs.com.au>
2019-09-09 15:04:13 +1000

    PS profile customisations

---
+++ Profile/DM-PowerShell_Profile.ps1
@@ -0,0 +1,30 @@
+# Profile script called by Microsoft.PowerShell_profile.ps1
+Set-Location -Path 'D:\Scripts\PowerShell'
+
+# Shell variables
+$Shell = $Host.UI.RawUI
+#$Shell.WindowTitle=""
+$KeyPath = "$Home\Documents\WindowsPowerShell"
+
+# Run the following sript to enable the Get-StoredCredential cmdlet from the KeyPath
+. ".\Profile\Functions-PSStoredCredentials.ps1"
+
+$BSize = $Shell.BufferSize
+$BSize.Width=120
+$BSize.Height=3000
+$Shell.BufferSize = $BSize
+
+$WSize = $Shell.WindowSize
+$WSize.Width=120
+$WSize.Height=40
+$Shell.WindowSize = $WSize
+
+# Show a custom message
+Write-Host -ForegroundColor 'White' "Running as $env:Username with profile path: $KeyPath `n"
+
+# Change the Shell Color
+#$shell.BackgroundColor = “Gray”
+#$shell.ForegroundColor = “Black”
+
+#To start with a clean Shell
+# Clear-Host
---
+++ Profile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+# This is the default profile loaded by PowerShell upon launch
+# It's normally located under: C:\Users\(UserName)\Documents\WindowsPowerShell
+#
+# This file is just used for setting the user-scope environment variable and launching the main script
+# Because I'm using multiple accounts, I just copy this into the location above for each one
+# Customisations are then handled in the shared script
+#
+# I'm using this method instead of a global profile as to not impact any other user accounts
+
+# Enter UPN of an Admin account for connecting to Office 365 with stored credentials
+$env:AdminUPN = ' '
+
+# Call the shared script from a common directory
+. "D:\Scripts\PowerShell\Profile\DM-PowerShell_Profile.ps1"
