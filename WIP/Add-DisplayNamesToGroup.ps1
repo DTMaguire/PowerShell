73716e43d19adaa50f8df15f8727583831c9d582
DTMaguire <Daniel.Maguire@nswlrs.com.au>
2019-11-08 18:32:15 +1100

    Updated to be more concise and use fewer variables

--- WIP/Add-DisplayNamesToGroup.ps1
+++ WIP/Add-DisplayNamesToGroup.ps1
@@ -1,30 +1,26 @@
 # PowerShell script to add a list of user Display Names to a group
 #
-# The Add-ADGroupMember cmdlet will only accept -Members of type:
+# The Add-ADGroupMember cmdlet will only accept input for '-Members' that are:
 #    Distinguished name
 #    GUID (objectGUID)
 #    Security identifier (objectSid)
 #    SAM account name (sAMAccountName)
 #
-# This script looks up SamAccountName for each Display Name and adds it to an array
-# Once complete, it runs Add-ADGroupMember and passes in the array as the -Members parameter
+# This script looks up the SamAccountName for each Display Name and adds it to an array
+# Once complete, it runs Add-ADGroupMember and passes in the array as the '-Members' parameter
 #
 # Version 1.0 - Copyright DM Tech 2019
 
 #Requires -Modules ActiveDirectory
 $AccountsArray = @()
 
-# Import a list of user display names from a plain txt file, assuming name on a new line
-$UserList = (Get-Content -Path "D:\Scripts\Input\DL List.txt")
-
+# Import a list of user display names from a plain txt file, assuming one name per line
 # Ensure no erroneous spaces in input, then sort in alphabetical order
-$DispNames = (($UserList).Trim() | Sort-Object)
+$DispNames = ((Get-Content -Path "D:\Scripts\Input\DL List.txt").Trim() | Sort-Object)
 
-# CSV import would look like this - just make sure it's in a valid comma-separated values format!
-#$UserList = (Import-Csv -Path "D:\Scripts\Input\Some.csv")
-
+# A CSV import would look like this - just make sure it's in a valid comma-separated values format!
 # Trim spaces again, but this uses .DisplayName to select the correct column in the CSV file
-#$DispNames = (($UserList.DisplayName).Trim() | Sort-Object)
+#$DispNames = (((Import-Csv -Path "D:\Scripts\Input\Some.csv").DisplayName).Trim() | Sort-Object)
 
 $Group = "Customer Facing Staff-1-667818328"
 
@@ -36,6 +32,7 @@ Write-Host -ForegroundColor 'Green' "`n`nTotal users: " $DispNames.Count "`n"
 foreach ($Name in $DispNames) {
 
     $UserAcc = (Get-ADUser -Filter {Name -like $Name} | Select-Object -ExpandProperty SamAccountName)
+
     # It's possible more than one SamAccountName is returned as Display Names do not have to be unique in AD
     # This array allows multiple returned accounts to be captured as individual items
     foreach ($Acc in $UserAcc) {
@@ -49,6 +46,7 @@ Write-Output $AccountsArray
 Write-Host -ForegroundColor 'Green' "`nTotal accounts matched: " $AccountsArray.Count "`n`n"
 
 if ((Read-Host -Prompt "Add listed users to `'${Group}`'? (y/N)") -eq "y") {
+
     # This passes in the AccountsArray in so each member gets added in one go - remove -WhatIf when ready!
     Add-ADGroupMember -Identity $Group -Members $AccountsArray -Confirm:$false -WhatIf
 }
