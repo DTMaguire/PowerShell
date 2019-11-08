3b508870ae3d05c8600505ee16b35215703e86ce
DTMaguire <Daniel.Maguire@nswlrs.com.au>
2019-11-08 18:18:15 +1100

    Update AD attributes when converting mailbox, account name check for script

--- Process-UserExits.ps1
+++ Process-UserExits.ps1
@@ -5,15 +5,15 @@
 #   Create parameters and turn into module
 #
 #Requires -PSEdition Desktop
-#Requires -Version 5.1
+#Requires -Version 5
 #Requires -Modules ActiveDirectory
 
 $NameList = @()
 $AccountsArray = @()
 $NotMatched = @()
-$MailDomain = "externalmaildomain.com.au"
-$ExchFQDN = "internalexchange.domain.name"
-$OutputDir = "\\SomeNetworkPath\ICT\ICT Operations\User Offboarding"
+$MailDomain = "nswlrs.com.au"
+$ExchFQDN = "sydawsexchange.trs.nsw"
+$OutputDir = "\\NAS-QS-TRS\Groups\Corp_Services\ICT\ICT Operations\User Offboarding"
 $LogTime = Get-Date -UFormat %y%m%d%H%M%S
 $LogPath = Join-Path -Path $OutputDir -ChildPath 'Logs'
 $LogName = Join-Path -Path $LogPath -ChildPath "$($LogTime).log"
@@ -35,20 +35,39 @@ If ($WinVer -lt [version]6.2) {
     $Shell.WindowSize = $WSize
 }
 
+if ($env:UserName -notlike "admin*") {
+    Write-Host -ForegroundColor 'Red' "`nThe current account `'$env:UserName`' does not appear to be a domain admin.
+Please re-run this script with your admin account (Shift-right click, Run as different user).`nExiting...`n"
+    Start-Sleep 3
+    exit
+}
+
 function GetUserAccount ($Name) {
 
     Get-ADUser -Filter {Name -like $Name -or SamAccountName -like $Name} -Properties * | `
-    Select-Object Name, SamAccountName, UserPrincipalName, Memberof, Enabled, Description, Info
+    Select-Object Name, SamAccountName, UserPrincipalName, Memberof, Enabled, Description, Info, msExchRemoteRecipientType
 }
 
-function ProcessMailbox ($AccountSAM, $AccountUPN) {
+function ProcessMailbox ($Account) {
 
+    $AccountUPN = $Account.UserPrincipalName
     if(Get-RemoteMailbox $AccountUPN -ErrorAction SilentlyContinue) {
-        Set-RemoteMailbox -Identity $AccountUPN -AcceptMessagesOnlyFrom $AccountUPN -HiddenFromAddressListsEnabled $True -WhatIf
-        Write-Host -ForegroundColor 'White' "`nSetting mailbox for `'$AccountUPN`' to shared..."
-        Invoke-Command -Session $O365Session -ScriptBlock {Set-Mailbox -Identity $Using:AccountUPN -Type Shared -WhatIf}
-        Set-ADUser $AccountSAM -Replace @{msExchRemoteRecipientType="100"; msExchRecipientTypeDetails="34359738368"} -WhatIf
 
+        Set-RemoteMailbox -Identity $AccountUPN -AcceptMessagesOnlyFrom $AccountUPN -HiddenFromAddressListsEnabled $True #-WhatIf
+        Write-Host -ForegroundColor 'White' "`nSetting mailbox for `'$($Account.UserPrincipalName)`' to shared..."
+        Invoke-Command -Session $O365Session -ScriptBlock {Set-Mailbox -Identity $Using:AccountUPN -Type Shared <#-WhatIf#>}
+
+        if ($Account.msExchRemoteRecipientType -eq 1) {
+            Write-Host -ForegroundColor 'White' "`nUpdating local AD recipient type value from `'1`' to `'97`'"
+            Set-ADUser $Account.SamAccountName -Replace @{msExchRemoteRecipientType="97"; msExchRecipientTypeDetails="34359738368"} #-WhatIf
+        } elseif ($Account.msExchRemoteRecipientType -eq 4) {
+            Write-Host -ForegroundColor 'White' "`nUpdating local AD recipient type value from `'4`' to `'100`'"
+            Set-ADUser $Account.SamAccountName -Replace @{msExchRemoteRecipientType="100"; msExchRecipientTypeDetails="34359738368"} #-WhatIf
+        } else {
+            Write-Host -ForegroundColor 'Magenta' "`nValue: `'$($Account.msExchRemoteRecipientType)' not valid for recipient type, skipping update..."
+            continue
+        }
+        
     } else {
         Write-Host -ForegroundColor 'Magenta' "`nNo mailbox for `'$AccountUPN`' found!"
     }
@@ -63,12 +82,12 @@ function RemoveFromGroups ($AccountSAM, $Groups) {
     foreach ($Group in $Groups) {
         Start-Sleep -Milliseconds 100
         Write-Host -ForegroundColor 'White' "Removing from:" $Group
-        Remove-ADGroupMember -Identity $Group -Member $AccountSAM -Confirm:$false -WhatIf
+        Remove-ADGroupMember -Identity $Group -Member $AccountSAM -Confirm:$false #-WhatIf
     }
 
     $FilePath = Join-Path -Path $OutputDir -ChildPath "UserExit_$($AccountSAM)_Groups.txt"
     Write-Host -ForegroundColor 'Green' "`nSaving to: $FilePath"
-    $Groups | Out-File $FilePath -NoClobber -WhatIf
+    $Groups | Out-File $FilePath -NoClobber #-WhatIf
 }
 
 function ProcessExit ($Account) {
@@ -95,21 +114,21 @@ function ProcessExit ($Account) {
 
     if ($Account.Enabled -eq $True) {
         Write-Host -ForegroundColor 'White' "`nDisabling account..."
-        Disable-ADAccount -Identity $AccountSAM -Confirm:$false -WhatIf
+        Disable-ADAccount -Identity $AccountSAM -Confirm:$false #-WhatIf
     } else {
         Write-Host -ForegroundColor 'Magenta' "`nAccount already disabled!"
     }
 
-    ProcessMailbox $AccountSAM $Account.UserPrincipalName
+    ProcessMailbox $Account
     
-    Set-ADUser -Identity $AccountSAM -Description $Account.Description -Replace @{info="$ProcessInfo`r`n$($Account.info)"} -WhatIf
+    Set-ADUser -Identity $AccountSAM -Description $Account.Description -Replace @{info="$ProcessInfo`r`n$($Account.info)"} #-WhatIf
 }
 
 If(!(Test-Path -Path $LogPath)){
     New-Item -Path $LogPath -ItemType Directory
 }
-Start-Transcript -Path $LogName | Out-Null
 
+Start-Transcript -Path $LogName | Out-Null
 Write-Host -ForegroundColor 'White' "`nStarting user exit script`n"
 
 do {
@@ -120,6 +139,12 @@ do {
 } until ($ReadInput -match '[\w.]+')
 
 $NameList = @(($ReadInput).Split(",").Trim())
+
+<#
+Alternatively, NameList can be set to get info from a file or even file names like so:
+$NameList = @((Get-Content -Path "D:\Scripts\Input\AWS-OldUsers_Remaining.txt") | Where-Object {$_ -ne "Jonathan Fan"})
+$NameList = @((Get-ChildItem -Path "\\NAS-QS-TRS\Groups\Corp_Services\ICT\ICT Operations\User Offboarding" | Select-Object -ExpandProperty Name) -replace ('UserExit_','') -replace ('_Groups.txt',''))
+#>
 
 Write-Host -ForegroundColor 'White' "`n`nImporting the following users:`n"
 Write-Output $NameList
@@ -174,11 +199,11 @@ if ($AccountsArray.Length -gt 0) {
                 $O365Cred = (Get-StoredCredential -UserName ($env:UserName + "@$MailDomain"))
             }
             catch {
-                $O365Cred = (Get-Credential -Credential ($env:UserName + "@$MailDomain"))
+                Write-Host -ForegroundColor 'Magenta' "`nAdmin credentials required..."
+                $O365Cred = (Get-Credential) #-Credential ($env:UserName + "@$MailDomain"))
             }
         
             Write-Host -ForegroundColor 'White' "`nOffice 365 Exchange Online session starting..."
-            
             $O365Session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
             -ConfigurationName Microsoft.Exchange -Credential $O365Cred -Authentication Basic -AllowRedirection
             $O365SessionCreated = $true
@@ -205,3 +230,4 @@ if ($O365SessionCreated -eq $true) {
 
 Write-Host -ForegroundColor 'White' "`nEnd of processing`n"
 Stop-Transcript | Out-Null
+$Shell.WindowTitle="User Offboarding Script"
