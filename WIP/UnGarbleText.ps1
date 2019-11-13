90d6cb62b301ec3fd067d75b97b9391bf43f33f8
DTMaguire <Daniel.Maguire@nswlrs.com.au>
2019-11-13 18:37:02 +1100

    String manipulation of txt file to create valid CSV for further processing

--- WIP/UnGarbleText.ps1
+++ WIP/UnGarbleText.ps1
@@ -1,8 +1,8 @@
 [System.Collections.ArrayList]$SourceFile = (Get-Content D:\Scripts\Input\SR-1058_GarbledMess.txt)
-
 while ($SourceFile -contains "`t") {
     $SourceFile.Remove("`t")
 }
+$OutputFile = 'D:\Scripts\Output\SR-1058_UnGarbled.csv'
 
 $Counter = 0
 foreach ($Line in $SourceFile) {
@@ -14,5 +14,7 @@ foreach ($Line in $SourceFile) {
     } else {
         $InputString = $Line + ","
     }
-    Out-File -FilePath 'D:\Scripts\Output\SR-1058_UnGarbled.csv' -InputObject $InputString -Append -NoNewline
+    Out-File -FilePath $OutputFile -Encoding UTF8 -InputObject $InputString -Append -NoNewline
 }
+
+Invoke-Item $OutputFile
