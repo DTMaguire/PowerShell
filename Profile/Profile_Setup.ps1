a7c8cb089333f0c2b1510a904e923df7fe355da3
DTMaguire <Daniel.Maguire@nswlrs.com.au>
2019-12-19 17:51:55 +1100

    Deleted

--- Profile/Profile_Setup.ps1
+++
@@ -1,8 +0,0 @@
-# Setup PowerShell Profile and Environment Variables
-# Run script under your domain admin account
-# Environment variables:
-# Get/Set Domain/UPNSuffix
-# Get/Set DevPath (currently D:\Scripts\PowerShell)
-# Set AdminUPN (run script and grab $Env:UserName + "@" + $Env:UPNSuffix)
-# Create sub folders in DevPath for Profile and Modules
-# Copy in shared PowerShell profile, PSStoredCredential function and relevant modules
