51e5551d408c3b68cc0fbd61ca5edb3b77fec842
admin Daniel Maguire <admin.dmaguire@nswlrs.com.au>
2020-02-14 13:20:33 +1100

    Major update

--- WIP/New-Users.ps1
+++ WIP/New-Users.ps1
@@ -3,80 +3,75 @@
 
 #Requires -Modules ActiveDirectory
 
-$NewUsers = @('Lindell Galaroza','Matthew Emanuel','Michael Cerna','Nick Craig')
+# Variables to set
+#$NewUsers = @(Get-Content ((Split-Path $Env:DevPath -Parent) + '\Input\NewUsers.txt')) # Read names from a text file
+$NewUsers = @('First Person') # Or enter names as an array here
+$CopyUser = 'DMaguire' # SAM account name of the user to copy
 
 # Common variables
-$CopyUser = 'CFunnell'
-$Password = (Get-Date).ToString("MMMMyyyy") # Formatted as 'January2020' (MonthYear)
-$HomeDrive = 'H:'
-$UserShare = '\\nas-qs-trs\users\'
-$Props = 'City','Company','Country','Department','Description','Manager','Office','PostalCode','State','StreetAddress','Title'
+$Props = 'City','Company','Country','Department','Description','HomeDrive','Manager','Office','PostalCode','State','StreetAddress','Title'
 
 $CopyObject = (Get-ADUser -Identity $CopyUser -Properties $Props)
-$CopyGroups = (Get-ADUser -Identity $CopyUser -Properties MemberOf | Select-Object -ExpandProperty MemberOf | Get-ADGroup)
+$CopyGroups = (Get-ADUser -Identity $CopyUser -Properties MemberOf | Select-Object -ExpandProperty MemberOf)
+
+# This places the new users in the same OU as the copied one, without it the account is created in the default Users OU
+#$OU = ($CopyObject.DistinguishedName.Split(',',2)[1])
+# 'January2020' (MonthYear)
+$Password = (ConvertTo-SecureString -AsPlainText ((Get-Date).ToString("MMMMyyyy")) -Force) 
+# SAM account name is appended for the H: drive folder so leave the trailing slash
+$UserShare = '\\nas-qs-trs\users\'
+
+$UserInstance = New-Object Microsoft.ActiveDirectory.Management.ADUser
+
+foreach ($Prop in $Props) {
+    if ($null -ne ${CopyObject}.$Prop) {
+        ${Script:UserInstance}.$Prop = ${CopyObject}.$Prop
+    } else {
+        Write-Host -ForegroundColor 'White' "No value set for $Prop on $CopyUser"
+    }
+}
+
+Write-Host -ForegroundColor 'White' "`nCommon parameters:"
+Format-List -InputObject $UserInstance
 
 foreach ($User in $NewUsers) {
-    
-    $UserInstance = New-Object Microsoft.ActiveDirectory.Management.ADUser
-    
-    $UserInstance.DisplayName = $User
-    $UserInstance.Name = $User
-    # Split the string into an array of 2 elements at the first ',' then select the element at array index [1]
-    #$UserInstance.DistinguishedName = ("CN=$User/" + ($CopyObject.DistinguishedName.Split(',',2)[1])) # Taking a punt here... 
 
-    $UserInstance.SamAccountName = (($User.Substring(0,1) + ($User.Split(' ')[-1])).ToLower())
-    $UserInstance.Given = ($User.Split(' ')[0])
-    $UserInstance.SurName = ($User.Split(' ')[-1])
-    $UserInstance.UserPrincipalName = (($User.Replace(' ','.') + '@' + $Env:UPNSuffix).ToLower())
-    $UserInstance.HomeDir = ($UserShare + $SAM)
+    $UserSAM = ($User.Substring(0,1) + ($User.Split(' ')[-1])).ToLower()
 
-
-        
-    foreach ($Prop in $Props) {
-        if ($null -ne $Prop) {
-            ($Script:UserInstance).$Prop = ($CopyObject).$Prop
-        }
+    $NewUserProps = [ordered]@{
+        Name = $User
+        DisplayName = $User
+        SamAccountName = $UserSAM
+        GivenName = ${User}.Split(' ')[0]
+        SurName = ${User}.Split(' ')[-1]
+        UserPrincipalName = (${User}.Replace(' ','.') + '@' + $Env:UPNSuffix).ToLower()
+        HomeDirectory = ($UserShare + $UserSAM)
     }
 
     Write-Host -ForegroundColor 'White' "`nSetting up user with details:`n"
-
-    Write-Host "OU:`t`t$OU`nSAM:`t`t$SAM`nGiven:`t`t$Given`nSurName:`t$SurName`nUPN:`t`t$UPN`nHomeDir:`t$HomeDir`n"
     
-    <# To create new users
-    New-ADUser -AccountPassword (ConvertTo-SecureString -AsPlainText $Password -Force) -ChangePasswordAtLogon $True `
-        -DisplayName $User -Enabled $True -GivenName $Given -HomeDirectory $HomeDir -HomeDrive $HomeDrive `
-        -Instance $Instance -Path $OU -SamAccountName $SAM -SurName $SurName -UserPrincipalName $UPN -WhatIf
-    #>
+    Format-Table -InputObject $NewUserProps
+    
+    #foreach($Key in $NewUserProps.Keys)
+    #{
+    #    $Value = $NewUserProps.$Key
+    #    Write-Output "$Key `t`t`t $Value"
+    #}
 
-    New-ADUser -SAMAccountName $SAM -Instance $userInstance -Path ($CopyObject.DistinguishedName.Split(',',2)[1])
+    #$NewUserProps | Out-String
 
-    # To update existing users
-    $ReplaceHashTable = @{
-        DisplayName = $User
-        HomeDirectory = $HomeDir
-        HomeDrive = $HomeDrive
-        UserPrincipalName = $UPN
-    }
+    New-ADUser @NewUserProps -Instance $UserInstance <#-Path $OU#> -AccountPassword $Password -Enabled $True -ChangePasswordAtLogon $True -WhatIf
+    
+    Write-Host -ForegroundColor 'White' "New mailboxes can be activated with the following command:"
+    Write-Host -ForegroundColor 'Cyan' "Enable-RemoteMailbox (${NewUserProps[UserPrincipalName]}) -RemoteRoutingAddress $(${User}.Replace(' ','.') + '@NSWLandRegistryServices.mail.onmicrosoft.com')`n"
+    #$RemoteMailbox = "Enable-RemoteMailbox ${NewUserProps}.UserPrincipalName -RemoteRoutingAddress (${User}.Replace(' ','.') + '@NSWLandRegistryServices.mail.onmicrosoft.com')"
 
-    $UpdateObject = (Get-ADUser -Identity $SAM -Properties $Props)
-
-    foreach ($Prop in $Props) {
-        ($Script:UpdateObject).$Prop = ($CopyObject).$Prop
-    }
-
-    Write-Host -ForegroundColor 'White' "Updated Object Properties:`n"
-    Write-Output $ReplaceHashTable
-    Write-Output $UpdateObject
-    Start-Sleep 3
-    Set-ADUser -Identity $SAM -ChangePasswordAtLogon $True -Replace $ReplaceHashTable -WhatIf
-    Start-Sleep 3
-    #Set-ADUser -Instance $UpdateObject
-    Write-Host -ForegroundColor 'Green' "`nCompleted: $User`n"
-    Start-Sleep 1
-
-    <#
-    $CopyGroups | ForEach-Object {Add-ADGroupMember -Identity $_ -Members $SAM -Verbose}
+    Write-Host -ForegroundColor 'White' "Waiting a few seconds for account object before starting groups...`n"
+    Start-Sleep 4
+    #$CopyGroups | ForEach-Object {Add-ADGroupMember -Identity $_.SamAccountName -Members $UserSAM -WhatIf}
     Write-Host -ForegroundColor 'Green' "`nUpdated Groups: $($CopyGroups.Count) `n"
     Start-Sleep 1
-    #>
+    
 }
+
+Write-Host -ForegroundColor 'Green' "User setup complete, wait a few minutes before running the Enable-RemoteMailbox command for each."
