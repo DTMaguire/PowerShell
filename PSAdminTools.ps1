44dfcf61e53a3881591d81f356a1b2f8ec5f7fcc
Daniel Maguire <Daniel.Maguire@nswlrs.com.au>
2020-06-16 14:55:38 +1000

    Rewrite for PowerShell 7, reduced form spacing

--- PSAdminTools.ps1
+++ PSAdminTools.ps1
@@ -1,34 +1,28 @@
 ﻿# PowerShell GUI form to launch Remote Server Admin Tools (RSAT) running with Domain Admin credentials 
-# Version 1.2 - Copyright DM Tech 2019
+# Version 1.4 - Copyright DM Tech 2020
 # 
-# This script enables a one-click launching of server admin tools from a workstation with standard domain user account
-# It works by looking for shortcuts (.lnk files) in the current directory and generating a list of buttons on a form
-# The form itself runs under domain admin credentials - the subsequent shortcuts then run in the same security context
+# This script enables a one-click launching of server admin tools from a workstation with standard domain user account.
+# It works by looking for shortcuts (.lnk files) in a specified directory and generating a list of buttons on a form.
+# The form itself runs under domain admin credentials - the subsequent shortcuts then run in the same security context.
 #
-# Usage: Copy regularly used links of Administrative Tools to a specified directory and update $ToolsPath below
+# Usage: Copy regularly used links of Administrative Tools to a specified directory and update $ToolsPath below.
 #
 # Check the %DEVPATH% environment variable exists and create a shortcut somewhere with the target (one line):
 #
 #    C:\Windows\System32\runas.exe /user:DOMAIN\ADAdminAccount <--Replace with your Domain Admin--<
-#    /savecred "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -File %DEVPATH%\PSAdminTools.ps1"
+#    /savecred "C:\Program Files\PowerShell\7\pwsh.exe -NoProfile -File %DEVPATH%\PSAdminTools.ps1"
 #
-# Open the shortcut (not the ps1 file), enter your password once to save it in the Windows Credential Vault
+# Open the shortcut (not the ps1 file), enter your password once to save it in the Windows Credential Vault.
+# Shortcuts can be added and removed from the $ToolsPath directory as required, but keep in mind your display height.
 #
-# Shortcuts can be added and removed from the $ToolsPath directory as required, but keep in mind your display height
+# The launch method doesn't work if the script or shortcuts are inside a user profile due to a Windows security feature.
+# The simple fix is to move it to another folder outside of your profile or another drive.
 #
-# The launch method doesn't work if the script or shortcuts are inside a user profile due to a Windows security feature
-# The simple fix is to move it to another folder outside of your profile or another drive
-#
-# A quick way to install the RSAT on Windows 10 is to run the script here: 
-#   https://gallery.technet.microsoft.com/Install-RSAT-for-Windows-75f5f92f
-#
-# Download and run it in an elevated PowerShell like this:> .\Install-RSATv1809v1903v1909.ps1 -All 
+# See also - PowerShell profile setup script which includes the RSAT Tools installation below:
+#   https://github.com/DTMaguire/PowerShell/blob/master/Profile/SetupPSProfile.ps1
 
-# Global variables
+# Script variables
 $ToolsPath = (Split-Path $Env:DevPath -Parent) + '\AdminTools'
-
-# Check OS Version to size form correctly
-$WinVer = [version](Get-CimInstance Win32_OperatingSystem).version
 
 Function HideConsoleWindow {
 
@@ -39,7 +33,6 @@ Function HideConsoleWindow {
         public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
 
     $ConsolePtr = [Console.Window]::GetConsoleWindow()
-
     [Console.Window]::ShowWindow($ConsolePtr, 0)
 }
 
@@ -48,67 +41,54 @@ Function GenerateForm {
     # Hide background console window
     HideConsoleWindow
 
-    Add-Type -AssemblyName System.Windows.Forms    
+    Add-Type -AssemblyName System.Windows.Forms
     Add-Type -AssemblyName System.Drawing
     
     # Generate Tools array
-    $Tools = @(Get-ChildItem -Path $ToolsPath | Where-Object -Property Name -like *.lnk | Select-Object Name,FullName)
+    $Tools = @(Get-ChildItem -Path $ToolsPath -Filter *.lnk | Select-Object Name,FullName)
 
-    # Create form sizing variables - can be reduced for a more compact interface
-    $ButtonHeight = 44
-    $ButtonYSpacing = 50
-    $TitleXPos = 0
-    $TitleYPos = ($ButtonYSpacing / 4)
-
-    # Windows 7 or earlier
-    If ($WinVer -lt [version]6.2) {
-
-        $FormWidth = 240
-        $ButtonWidth = ($FormWidth - 40)
-        $ButtonXPos = ($FormWidth - $ButtonWidth) / 4 # Looks about right
-        $FormHeight = ($ButtonHeight * 2) + ($ButtonYSpacing * $Tools.Count)
-        $TitleFont = 'Microsoft Sans Serif, 9'
-    }
-    Else {
-
-        $FormWidth = 258
-        $ButtonWidth = ($FormWidth - 40)
-        $ButtonXPos = ($FormWidth - $ButtonWidth) / 3.6 # Looks about right
-        $FormHeight = ($ButtonHeight * 2.25) + ($ButtonYSpacing * $Tools.Count)
-        $TitleFont = 'Microsoft Sans Serif, 10'
-    }
+    # Create form sizing variables
+    $FormWidth = 254
+    $ButtonHeight = 40
+    $ButtonYSpacing = 44
+    $TitleXPos = 2
+    $TitleYPos = ($ButtonYSpacing / 3)
+    $ButtonWidth = ($FormWidth - 32)
+    $ButtonXPos = ($FormWidth - $ButtonWidth) / 4
+    $FormHeight = ($ButtonHeight * 2) + ($ButtonYSpacing * $Tools.Count) + 3
+    $TitleFont = [System.Drawing.Font]::new('Microsoft Sans Serif', 10)
 
     # Build form
-    $Form = New-Object System.Windows.Forms.Form
-    $Form.Text = "PSAdminTools"
-    $Form.Size = New-Object System.Drawing.Size($FormWidth,$FormHeight)
-    $Form.Topmost = $False   # Sets to always on top
-    $Form.FormBorderStyle = 'Fixed3D'
+    $Form = [System.Windows.Forms.Form]::new()
+    $Form.Size = [System.Drawing.Size]::new($FormWidth,$FormHeight)
+    $Form.FormBorderStyle = 'FixedDialog'
+    $Form.Text = 'PSAdminTools'
     $Form.MaximizeBox = $False
+    $Form.Topmost = $False   # Sets to always on top
 
     # Add title
-    $Title = New-Object system.Windows.Forms.Label
+    $Title = [System.Windows.Forms.Label]::new()
     $Title.Text = "DM's PowerShell AdminTools Launcher"
     $Title.AutoSize = $True
     $Title.Font = $TitleFont
-    $Title.Location = New-Object System.Drawing.Point($TitleXPos,$TitleYPos)
+    $Title.Location = [System.Drawing.Point]::new($TitleXPos,$TitleYPos)
     $Form.Controls.Add($Title)
 
     # Dynamically add Buttons
     ForEach ($Tool in $Tools) {
 
             $ButtonYPos = $ButtonHeight + ($ButtonYSpacing * $Tools.IndexOf($Tool))
-            $Button = New-Object System.Windows.Forms.Button
-            $Button.Location = New-Object System.Drawing.Size($ButtonXPos,$ButtonYPos)
-            $Button.Size = New-Object System.Drawing.Size($ButtonWidth,$ButtonHeight)
+            $Button = [System.Windows.Forms.Button]::new()
+            $Button.Location = [System.Drawing.Size]::new($ButtonXPos,$ButtonYPos)
+            $Button.Size = [System.Drawing.Size]::new($ButtonWidth,$ButtonHeight)
             $Button.Text = $Tool.Name -replace ('.lnk','')
             $LaunchTool = [System.Management.Automation.ScriptBlock]::Create("Invoke-Item '$($Tool.FullName)'")
             $Button.Add_Click($LaunchTool)
             $Form.Controls.Add($Button)
      }
 
-    # Show the Form 
-    $Form.ShowDialog()| Out-Null 
+    # Finally, show the Form 
+    $Form.ShowDialog()
 }
 
 GenerateForm
